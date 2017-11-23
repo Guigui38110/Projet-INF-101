1202ce555c760baa1b7a4fd05e0d5d94c4af2620
Guigui38110 <33932284+Guigui38110@users.noreply.github.com>
2017-11-23 16:05:36 +0100

    Update Projet Info code.py

--- Projet Info code.py
+++ Projet Info code.py
@@ -6,9 +6,9 @@ from random import *
 paquet_melange = []
 paquet_range= []
 main_joueur1=[]
-i = 1
+i = 0
 
-while i < 109 :
+while i < 108 :
     paquet_range.append(i)
     i += 1
 
@@ -21,29 +21,29 @@ def initialisation(liste) :  #Mélange de la liste de chiffres rangés pour y pl
 
 def couleur(indice) : #Détermine la couleur en fonction du numéro de la carte piochée
     if 1 <= indice <= 25 :
-        return "Rouge"
+        return " Rouge"
     if 26 <= indice <= 50 :
-        return "Bleu"
+        return " Bleu"
     if 51 <= indice <= 75 :
-        return "Jaune"
+        return " Jaune"
     if 76 <= indice <= 100 :
-        return "Vert"
+        return " Vert"
     if 101 <= indice <= 108 :
-        return "Multicolore"
+        return " Multicolore"
 
 def numero(indice) : # Permet de renvoyer le numéro (correspondance) "physique" de la carte par rapport au numéro pioché 
-    if couleur(indice) == "Multicolore" :
+    if couleur(indice) == " Multicolore" :
         if 101 <= indice <= 104 :
             return 13
         else :
             return 14
-    if couleur(indice) == "Vert" :
+    if couleur(indice) == " Vert" :
         indice = indice - 75
-    if couleur(indice) == "Jaune" :
+    if couleur(indice) == " Jaune" :
         indice = indice - 50
-    if couleur(indice) == "Bleu" :
+    if couleur(indice) == " Bleu" :
         indice = indice - 25
-    if couleur(indice) == "Rouge" :
+    if couleur(indice) == " Rouge" :
         if indice == 1 :
             return 0
         if 2 <= indice <= 3 :
@@ -89,19 +89,21 @@ def texte(indice) : #Fait la correspondance entre le numéro de la carte pioché
     if a == 12 :
         a = "Passe le tour"
         x = a + b
-    if 0 <= a <= 9 :
+    else :
         a = str(a)
         x = a + b
     return x
 
 def pioche(liste) : #Permet de piocher une carte et la retire de la liste pour ne pas piocher 2 fois la même
-    carte_pioche = randint(1, 108)
+    carte_pioche = randint(0, len(liste)-1)
+    print(carte_pioche)
     liste.remove(carte_pioche)
-    carte_pioche = texte(numero(carte_pioche))
+    carte_pioche = texte(carte_pioche) #Passe dans le convertisseur 
     return carte_pioche #Carte piochée
 
 def piocher(paquet, main_joueur, carte_a_piocher): #Tant que n est inférieure au nombre de carte à piocher, on pioche et on rempli la main du joueur en s'aidant de la fonction "pioche"
 	n=0
+	
 	while n < carte_a_piocher:
 		carte = pioche(paquet_melange)
 		main_joueur.append(carte)
@@ -110,3 +112,4 @@ def piocher(paquet, main_joueur, carte_a_piocher): #Tant que n est inférieure a
 initialisation(paquet_melange)
 piocher(paquet_melange, main_joueur1, 7)
 print(main_joueur1)
+
