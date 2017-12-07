cf2bf323d6b0a0b2f39dfcb34bfd7d7cc662b267
JacquetR <33932301+JacquetR@users.noreply.github.com>
2017-12-07 17:27:09 +0100

    Update Projet Info code.py

--- Projet Info code.py
+++ Projet Info code.py
@@ -5,8 +5,9 @@ paquet_melange = []
 paquet_range= []
 main_joueur1=[]
 main_cache_joueur1 = []
-pile_de_jeu = [5, 8, 9, 10]
+pile_de_jeu = [5, 8, 9, 24]
 sens = 1
+liste_joueur=[]
 i = 1
 
 while i < 109 :
@@ -170,10 +171,9 @@ def gestionjoueur ():
 		liste_liste_joueur.append(liste_vide)
 		i=i+1
 
-	liste_joueur=[]
 	i=1
 	while i <= nb_joueur:
-		nom=input("Entrez le nom du joueur", i, ".")
+		nom=input("Entrez le nom du joueur n°"+str(i))
 		liste_joueur.append(nom)
 		i=i+1
 
@@ -182,60 +182,63 @@ def gestionjoueur ():
 		dico_joueur[liste_joueur[j]]=liste_liste_joueur[j]
 		j=j+1
 
-def sens_rotation(liste_des_joueurs, liste_pile, dernier_joueur, parcoureur) :
+def sens_rotation(liste_des_joueurs, liste_pile, dernier_joueur, sens) :
     derniere_carte = liste_pile[-1]
     i = -1 #indice du dernier joueur
     x = ",,"
     while x != dernier_joueur :
             i += 1
             x = liste_des_joueurs[i]
-if sens == 1 :
-    if numero(derniere_carte) == 11 :
-        sens = sens * (-1)
-        if i = 0 :
-            i = len(liste_des_joueurs) - 1
-            prochain_joueur = liste_des_joueurs[i]
+    if sens == 1 :
+        if numero(derniere_carte) == 11 :
+            sens = sens * (-1)
+            if i == 0 :
+                i = len(liste_des_joueurs) - 1
+                prochain_joueur = liste_des_joueurs[i]
+            else :
+                prochain_joueur = liste_des_joueurs[i-1]
+        elif numero(derniere_carte) == 12 :
+            i += 2
+            if i == len(liste_des_joueurs) +1 :
+                i = 1
+                prochain_joueur = liste_des_joueurs[i]
+            elif i == len(liste_des_joueurs) :
+                i = 0
+                prochain_joueur = liste_des_joueurs[i]
+            else :
+                prochain_joueur = liste_des_joueurs[i]
         else :
-            prochain_joueur = liste_des_joueurs[i-1]
-    elif numero(derniere_carte) == 12 :
-        i += 2
-        if i = len(liste_des_joueurs) :
-            i = 0
-            prochain_joueur = liste_des_joueurs[i]
-        elif i = len(liste_des_joueurs) + 1 :
-            i = 1
-            prochain_joueur = liste_des_joueurs[i]
-    else :
-        if i = len(liste_des_joueurs) - 1 :
-            i = 0
-            prochain_joueur = liste_des_joueurs[i]
+            if i == len(liste_des_joueurs) - 1 :
+                i = 0
+                prochain_joueur = liste_des_joueurs[i]
+            else :
+                prochain_joueur = liste_des_joueurs[i+1]
+    if sens == -1 :
+        if numero(derniere_carte) == 11 :
+            sens = sens * (-1)
+            if i == len(liste_des_joueurs) - 1 :
+                i = 0
+                prochain_joueur = liste_des_joueurs[i]
+            else :
+                prochain_joueur = liste_des_joueurs[i+1]
+        elif numero(derniere_carte) == 12 :
+            i += -2
+            if i == -2 :
+                i = len(liste_des_joueurs) - 2
+                prochain_joueur = liste_des_joueurs[i]
+            elif i == -1 :
+                i = len(liste_des_joueurs) - 1
+                prochain_joueur = liste_des_joueurs[i]
         else :
-            prochain_joueur = liste_des_joueurs[i+1]
-if sens == -1 :
-    if numero(derniere_carte) == 11 :
-        sens = sens * (-1)
-        if i = len(liste_des_joueurs) - 1 :
-            i = 0
-            prochain_joueur = liste_des_joueurs[i]
-        else :
-            prochain_joueur = liste_des_joueurs[i+1]
-    elif numero(derniere_carte) == 12 :
-        i += -2
-        if i = 0 :
-            i = len(liste_des_joueurs) - 2
-            prochain_joueur = liste_des_joueurs[i]
-        elif i = 1 :
-            i = len(liste_des_joueurs) - 1
-            prochain_joueur = liste_des_joueurs[i]
-    else :
-        if i = 0 :
-            i = len(liste_des_joueurs) - 1
-            prochain_joueur = liste_des_joueurs[i]
-        else :
-            prochain_joueur = liste_des_joueurs[i-1]
-return prochain_joueur
+            if i == 0 :
+                i = len(liste_des_joueurs) - 1
+                prochain_joueur = liste_des_joueurs[i]
+            else :
+                prochain_joueur = liste_des_joueurs[i-1]
+    return prochain_joueur
     
 initialisation(paquet_melange)
 gestionjoueur()
 dernier_joueur = liste_joueur[0]
-piocher(paquet_melange, main_joueur1, 5)
+print(sens_rotation(liste_joueur, pile_de_jeu, dernier_joueur, sens))
+
